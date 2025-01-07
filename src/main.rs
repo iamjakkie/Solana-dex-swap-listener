1234039f3b6b7ce17a70aa8cf2aa4b2ae40a74e5
jakkie <jakubkielbasiewicz@gmail.com>
2025-01-07 09:49:02 +0100

    helper method to parse single tx

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,20 @@ use std::time::Instant;
 
 use rpc_client::{fetch_block_with_version, get_latest_slot};
 use block_processor::process_block;
+use tx_processor::process_tx;
+use utils::get_amm_data;
 
 
 #[tokio::main]
 async fn main() {
+    let tx = "3GFsViNSsRkVGVky7GYYq9VT5XnHJDsSsa1jUtjEs8jYpuF327rB6FU9e4hzto5pG5SuT9essSDDbBnDPd8Wtq66";
+    let encoded_tx = rpc_client::get_signature(tx).await.unwrap();
+    let td = process_tx(encoded_tx.transaction, encoded_tx.slot, encoded_tx.block_time.unwrap()).await.unwrap();
+    let trade = td.first().unwrap();
+    get_amm_data(&trade.pool_address);
+    return;
+
+
     // let block_slot = 281418454;
     // let block = fetch_block_with_version(block_slot).await.unwrap();
 
@@ -21,8 +31,8 @@ async fn main() {
     let mut last_processed_slot: Option<u64> = None;
 
     loop {
-        let latest_slot = get_latest_slot().await.expect("Failed to get latest slot");
-        // let latest_slot = 312269072;
+        // let latest_slot = get_latest_slot().await.expect("Failed to get latest slot");
+        let latest_slot = 312300763;
         // println!("Latest slot: {}", latest_slot);
 
         let start_slot = match last_processed_slot {
--- src/rpc_client.rs
+++ src/rpc_client.rs
@@ -1,6 +1,8 @@
+use std::str::FromStr;
+
 use solana_client::{client_error::ClientError, rpc_client::RpcClient};
-use solana_sdk::commitment_config::{CommitmentConfig, CommitmentLevel};
-use solana_transaction_status::EncodedConfirmedBlock;
+use solana_sdk::{bs58, commitment_config::{CommitmentConfig, CommitmentLevel}, signature::Signature};
+use solana_transaction_status::{EncodedConfirmedBlock, EncodedConfirmedTransactionWithStatusMeta, EncodedTransactionWithStatusMeta};
 use anyhow::{Error, Result};
 use serde_json::json;
 use crate::global::RPC_CLIENT;
@@ -26,4 +28,19 @@ pub async fn get_latest_slot() -> Result<u64, ClientError> {
     let slot = rpc_client.get_slot_with_commitment(CommitmentConfig::confirmed());
     // let response: serde_json::Value = rpc_client.send(solana_client::rpc_request::RpcRequest::GetSlot, json!([]))?;
     slot
+}
+
+pub async fn get_signature(tx: &str) -> Result<EncodedConfirmedTransactionWithStatusMeta, Error> {
+    let rpc_client = RPC_CLIENT.clone();
+    let signature = Signature::from_str(tx).unwrap();
+    let params = json!([
+        tx,
+        { "maxSupportedTransactionVersion": 0 }
+    ]);
+    let res = rpc_client.send(solana_client::rpc_request::RpcRequest::GetTransaction, params)?;
+    // let res = rpc_client.get_transaction(&signature, solana_transaction_status::UiTransactionEncoding::Base58)?;
+    // let response: serde_json::Value = rpc_client.send(solana_client::rpc_request::RpcRequest::GetSignatureStatus, json!([tx]))?;
+    // let block: EncodedConfirmedBlock = serde_json::from_value(response)?;
+
+    Ok(res)
 }
