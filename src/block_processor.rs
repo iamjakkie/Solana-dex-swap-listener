b605fa2ca1d553ac1f3daaaa32a7303c4746a1d2
jakkie <jakubkielbasiewicz@gmail.com>
2025-01-14 10:17:16 +0100

    interim data checks

--- src/block_processor.rs
+++ src/block_processor.rs
@@ -1,13 +1,14 @@
-use std::collections::{HashMap, HashSet};
-use std::os::unix::process;
 
-use crate::trade_parser::get_trade_instruction;
+
+use std::{thread::current, time::{SystemTime, UNIX_EPOCH}};
+
 use crate::{
     models::{TokenBalance, TradeData, UiTokenAmount},
     tx_processor::process_tx,
     utils::{convert_to_date, get_amt, get_mint, get_signer_balance_change, save_to_csv},
 };
-use solana_sdk::{bs58, commitment_config::CommitmentConfig};
+use std::time::Duration;
+use chrono::{DateTime, Utc};
 use solana_transaction_status::{EncodedConfirmedBlock, UiInnerInstructions};
 
 pub async fn process_block(block: EncodedConfirmedBlock) {
@@ -15,7 +16,12 @@ pub async fn process_block(block: EncodedConfirmedBlock) {
     let slot = block.parent_slot;
     let mut data: Vec<TradeData> = vec![];
 
-    // println!("Transactions: {}", block.transactions.len());
+    // convert timestamp to human readable timestamp
+    let d = UNIX_EPOCH + Duration::from_secs(timestamp.try_into().unwrap());
+    // Create DateTime from SystemTime
+    let datetime = DateTime::<Utc>::from(d);
+    // Formats the combined date and time with the specified format string.
+    let timestamp_str = datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string();
 
     for trx in block.transactions {
         match process_tx(trx, slot, timestamp).await {
@@ -25,6 +31,12 @@ pub async fn process_block(block: EncodedConfirmedBlock) {
             None => {}
         }
     }
+
+    let current_time = SystemTime::now();
+    let current_datetime = DateTime::<Utc>::from(current_time);
+    let current_timestamp_str = current_datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string();
+
+    println!("Block time: {:?}, processed at: {:?}", timestamp_str, current_timestamp_str);
 
     // println!("Length of data: {}", data.len());
 
