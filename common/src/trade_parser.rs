0cd2de1d4505a463dc8346b299cd2a99390a7046
jakkie <jakubkielbasiewicz@gmail.com>
2025-03-21 17:11:13 +0100

    meteora dlmm

--- common/src/trade_parser.rs
+++ common/src/trade_parser.rs
@@ -1,8 +1,13 @@
-use solana_sdk::pubkey::Pubkey;
 use solana_transaction_status::UiInstruction;
 
 use crate::models::{TokenBalance, TradeInstruction};
 use crate::utils::prepare_input_accounts;
+
+const SWAP_WITH_PARTNER_DISCRIMINATOR: u64 = u64::from_le_bytes([248, 198, 158, 145, 225, 117, 135, 200]);
+const SWAP_DISCRIMINATOR: u64 = u64::from_le_bytes([248, 198, 158, 145, 225, 117, 135, 200]);
+const SWAP_EXACT_OUT_DISCRIMINATOR: u64 = u64::from_le_bytes([250, 73, 101, 33, 38, 207, 75, 184]);
+const SWAP_WITH_PRICE_IMPACT_DISCRIMINATOR: u64 =
+    u64::from_le_bytes([56, 173, 230, 208, 173, 228, 156, 205]);
 
 fn parse_raydium_trade_instruction(
     bytes_stream: &Vec<u8>,
@@ -54,10 +59,10 @@ fn parse_meteora_trade_instruction(
 
     let mut result: Option<TradeInstruction> = None;
 
-    let swap_with_partner_discriminator: u64 = u64::from_le_bytes([248, 198, 158, 145, 225, 117, 135, 200]);
+    
 
     match discriminator {
-        swap_with_partner_discriminator => {
+        SWAP_WITH_PARTNER_DISCRIMINATOR => {
             result = Some(TradeInstruction {
                 dapp_address: String::from("Eo7WjKq67rjJQSZxS6z3YkapzY3eMj6Xy8X5EQVn5UaB"),
                 name: String::from("Swap"),
@@ -74,9 +79,50 @@ fn parse_meteora_trade_instruction(
 }
 
 fn parse_meteora_dlmm_trade_instruction(
-
+    bytes_stream: &Vec<u8>,
+    input_accounts: Vec<String>,
 ) -> Option<TradeInstruction> {
-    
+    let (disc_bytes, rest) = bytes_stream.split_at(8);
+    let disc_bytes_arr: [u8; 8] = disc_bytes.to_vec().try_into().unwrap();
+    let discriminator: u64 = u64::from_le_bytes(disc_bytes_arr);
+
+    let mut result = None;
+
+    match discriminator {
+        SWAP_DISCRIMINATOR => {
+            result = Some(TradeInstruction {
+                dapp_address: String::from("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"),
+                name: String::from("Swap"),
+                amm: input_accounts.get(0).unwrap().to_string(),
+                vault_a: input_accounts.get(2).unwrap().to_string(),
+                vault_b: input_accounts.get(3).unwrap().to_string(),
+                ..Default::default()
+            });
+        },
+        SWAP_EXACT_OUT_DISCRIMINATOR => {
+            result = Some(TradeInstruction {
+                dapp_address: String::from("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"),
+                name: String::from("SwapExactOut"),
+                amm: input_accounts.get(0).unwrap().to_string(),
+                vault_a: input_accounts.get(2).unwrap().to_string(),
+                vault_b: input_accounts.get(3).unwrap().to_string(),
+                ..Default::default()
+            });
+        },
+        SWAP_WITH_PRICE_IMPACT_DISCRIMINATOR => {
+            result = Some(TradeInstruction {
+                dapp_address: String::from("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"),
+                name: String::from("SwapWithPriceImpact"),
+                amm: input_accounts.get(0).unwrap().to_string(),
+                vault_a: input_accounts.get(2).unwrap().to_string(),
+                vault_b: input_accounts.get(3).unwrap().to_string(),
+                ..Default::default()
+            });
+        },
+        _ => {}
+    }
+
+    return result;
 }
 
 pub fn get_trade_instruction(
@@ -114,7 +160,8 @@ pub fn get_trade_instruction(
         },
         "LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo" => {
             result = parse_meteora_dlmm_trade_instruction(
-
+                &instruction_data,
+                input_accounts,
             )
         },
         _ => {}
